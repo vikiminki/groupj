bb78369eedb592b5f19f57d62046f38ae6d64b8e
slumpmassigt <victoriabull@live.se>
2018-09-25 11:39:38 +0200

    phase 1 kinda finished

--- first.py
+++ first.py
@@ -2,30 +2,54 @@ from userinterface import userinterface
 
 class game_manager:
     phase = 1
-    state = []
-    player_one = 9
-    player_two = 9
+    player_one = {"name": "", "color": "black", "stones": 1}
+    player_two = {"name": "", "color": "white", "stones": 1}
+    turn = 1
+    ui = userinterface()
 
-    def change_phase(self, new_phase):
-        self.phase = new_phase
-    
     def init_game(self):
-        print "So you want to play huh?"
-        print "Press 1 or 2 to pick game mode"
+        print("So you want to play huh?")
+        print("Press 1 or 2 to pick game mode")
         choice = input("Play versus player (1) or play versus computer(2):")
-        if(choice == 2):
-            print "Game mode not supported in this version"
-        if(choice == 1):
-            player1 = raw_input("Enter name for player 1:")
-            player2 = raw_input("Enter name for player 2:")
-            #"load board"
-            print "Game starting up"
-            ui = userinterface()
-            ui.print_board()
+        if choice == "2":
+            print("Game mode not supported in this version")
+        if choice == "1":
+            player1 = input("Enter name for player 1:")
+            player2 = input("Enter name for player 2:")
+            self.player_one["name"] = player1
+            self.player_two["name"] = player2
+            print("Game starting up")
+            print(player1 ,"is black, ", player2 , " is white.")
+            if(self.phase == 1):
+                self.place()
+            else:
+                self.move()
 
-    def init_move_options(self, player1, player2):
-        move = move_options()
-        move.give_options()
+    def place(self):
+        self.ui.print_board()
+        print("\n")
+        print("To place a stone type the position you want to place a stone in, f.e g7: \n")
+        self.ui.print_board_move()
+        place = input("Make move: \n")
+        if(self.ui.legal_move(place)):
+            if(self.turn % 2 == 1):
+                self.ui.make_move(place, "B"+ str(self.player_one["stones"]))
+                self.player_one["stones"] += 1
+            else:
+                self.ui.make_move(place, "W" + str(self.player_two["stones"]))
+                self.player_two["stones"] += 1
+            self.turn += 1
+            if((self.player_one["stones"] > 9) and (self.player_two["stones"] >9)):
+                self.phase = 2
+                print("Phase 2!")
+            else:
+                self.place()
+        else:
+            print("The move you are trying to make is not legal! Try again")
+            self.place()
+    
+    #def move(self): 
+        
 
 obj = game_manager()
 obj.init_game()
